00dcc28c04303e4ec06874e58549d47820be355d
Dariusz Bernys <dariusz.bernys@ecs.co.uk>
2021-07-20 12:25:15 +0200

    finished backend

--- bankApp.py
+++ bankApp.py
@@ -1,7 +1,8 @@
 import logging
 from flask import Flask, session, render_template, redirect, url_for, request
 from flask_sqlalchemy import SQLAlchemy
-import time
+from uuid import uuid4
+import datetime
 
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///bank.db"
@@ -9,11 +10,15 @@ app.secret_key = "Elohell"
 db = SQLAlchemy(app)
 logging.basicConfig(level=logging.DEBUG)
 
+approvers = ['Admin', 'Tommy']
+
 
 class Transaction(db.Model):
     id = db.Column(db.String(100), primary_key=True)
-    currency = db.Column(db.String(1), nullable=False)
+    currency = db.Column(db.String(3), nullable=False)
     value = db.Column(db.Float, nullable=False)
+    tran_date = db.Column(db.Date, nullable=False)
+    approve_date = db.Column(db.Date, nullable=True)
     madeby = db.Column(db.String(200), nullable=False)
     approvedby = db.Column(db.String(200), nullable=True)
 
@@ -29,15 +34,68 @@ def index():
 
 @app.route("/dashboard")
 def dashboard():
-    # transactions = Transaction.query.all()
-    transactions = [Transaction(
-        id=2, currency="$", value=1238.0, madeby="Luke", approvedby="skywalker")]
-    return render_template("dashboard.html", user=session['user'], transactions=transactions)
+    transactions = Transaction.query.all()
+    able_to_approve = (session['user'] in approvers)
+    return render_template("dashboard.html", user=session['user'], transactions=transactions, able_to_approve=able_to_approve)
 
 
-@app.route("/create-transaction")
+@ app.route("/create-transaction", methods=['GET', 'POST'])
 def create_transaction():
-    return "almost there"
+    if request.method == 'POST':
+        transaction = Transaction(
+            id=str(uuid4()),
+            currency=request.form['currency'],
+            value=request.form['value'],
+            tran_date=datetime.datetime.now(),
+            madeby=session['user']
+        )
+
+        try:
+            db.session.add(transaction)
+            db.session.commit()
+        except Exception as e:
+            return f"well... why don't you just fuck off -- {str(e)}"
+
+        else:
+            return render_template("create_transaction.html")
+    else:
+        return render_template("create_transaction.html")
+
+
+@app.route("/approve/<id>")
+def approve(id):
+    tran = Transaction.query.get_or_404(id)
+
+    tran.approve_date = datetime.datetime.now()
+    tran.approvedby = session['user']
+
+    try:
+        db.session.commit()
+        return redirect(url_for("dashboard"))
+
+    except Exception as e:
+        return f"Well why don't you just fuck off -- {str(e)}"
+
+
+@app.route("/cancel/<id>")
+def cancel(id):
+    tran = Transaction.query.get_or_404(id)
+
+    try:
+        db.session.delete(tran)
+        db.session.commit()
+        return redirect(url_for("dashboard"))
+
+    except Exception as e:
+        return f"Well why don't you just fuck off -- {str(e)}"
+
+
+@app.route("/logout")
+def logout():
+    if 'user' in session:
+        session.pop('user', None)
+
+    return redirect(url_for("login"))
 
 
 @app.route("/login", methods=['GET', 'POST'])
